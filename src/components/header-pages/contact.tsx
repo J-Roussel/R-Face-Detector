2fd9cfd370bdd5fa306796ea38d7cd169a8d68ef
J-Roussel <hei.roussel.2@gmail.com>
2022-08-04 03:57:56 +0300

    update contact page

--- src/components/header-pages/contact.tsx
+++ src/components/header-pages/contact.tsx
@@ -4,9 +4,15 @@ import './contact.css';
 const Contact:React.FunctionComponent = ():JSX.Element => {
 
     return(
-        <div className='container-input-contact'>
-   
-        </div>
+        <>
+            <div className='container-input-contact'>
+                <p id='text-contact'>Open Up - Write and tell us your views - we are open to criticism, suggestions for improvement, questions, and of course we also like to receive praise... </p>
+            </div>
+            <div className="container-logo-contact">
+                <p className='copyright'>&copy; J.Roussel Rakotonirina</p>
+                <p className='copyright'>STD21050</p>
+            </div>
+        </>
     );
 }
 
