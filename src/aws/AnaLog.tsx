d309b70fe4b0501952160f3fceb8a07e2cc4ed93
J-Roussel <hei.roussel.2@gmail.com>
2022-08-04 03:08:40 +0300

    update .env and AnaLog

--- src/aws/AnaLog.tsx
+++ src/aws/AnaLog.tsx
@@ -2,8 +2,9 @@ import AWS from "aws-sdk";
 
 //
 export const AnaLog = () => {
-    AWS.config.region= process.env.region as string;
+    AWS.config.region= process.env.REACT_APP_REGION as string;
     AWS.config.credentials = new AWS.CognitoIdentityCredentials( {
-        IdentityPoolId: process.env.IdentityPoolId as string
+        IdentityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID as string
     })
+
 }
